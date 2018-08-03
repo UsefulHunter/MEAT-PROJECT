55d053fb601b61f48d0932f6f071774e7d1e4c14
Brian Icaro D. Marques <bi.dm@hotmail.com>
2018-08-03 16:57:44 -0300

    Atualizado para o ANGULAR 6
    OHHH  MY GOOOOOOOOD,
    Tentando buscar o @angular/animations perdido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { NgModule, LOCALE_ID, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, PreloadAllModules } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
-import { LocationStrategy, HashLocationStrategy } from '@angular/common'
+import { LocationStrategy, HashLocationStrategy, registerLocaleData } from '@angular/common'
+import localePt from '@angular/common/locales/pt'
 import { ApplicationErrorHandler } from './app.error-handler'
 import { ROUTES } from './app.routes'
+
+registerLocaleData(localePt, 'pt')
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -51,7 +54,7 @@ import { UserDetailComponent } from './header/user-detail/user-detail.component'
     SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules}),
   ],
-  providers: [ {provide: LocationStrategy, useClass: HashLocationStrategy}, {provide: LOCALE_ID, useValue: 'pt-BR'},
+  providers: [ {provide: LocationStrategy, useClass: HashLocationStrategy}, {provide: LOCALE_ID, useValue: 'pt'},
                 {provide: ErrorHandler, useClass: ApplicationErrorHandler}],
   bootstrap: [AppComponent]
 })
